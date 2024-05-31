163306e8cbe05bd075ca716729f7c229c18249d3
Justin Grego <97710986+JGrego-AWS@users.noreply.github.com>
2024-05-31 07:30:06 -0400

    Add licenses

--- Windows/INSTALL/FN04_Windows_Updates.py
+++ Windows/INSTALL/FN04_Windows_Updates.py
@@ -1,3 +1,19 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import logging
 import boto3
 import winrm
@@ -60,7 +76,7 @@ def lambda_handler(event, context):
 
     # Install PSWindowsUpdate module https://www.powershellgallery.com/packages/PSWindowsUpdate/
     logger.info("Loading PSWindowsUpdate PowerShell module.")
-    result = session.run_ps(
+    _result = session.run_ps(
         "Set-ExecutionPolicy Bypass;Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force;Install-Module -Name PSWindowsUpdate -Force"
     )
 
@@ -71,11 +87,11 @@ def lambda_handler(event, context):
         + ImageBuilderHostname
         + " -Script {ipmo PSWindowsUpdate; Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot -Verbose | Out-File C:\Windows\PSWindowsUpdate.log } -RunNow -Confirm:$false -Verbose -ErrorAction Ignore"
     )
-    result = session.run_ps(UpdateCommand)
+    _result = session.run_ps(UpdateCommand)
 
     # Return PowerShell execution policy to Windows default
     logger.info("Resetting PowerShell ExecutionPolicy.")
-    result = session.run_ps("Set-ExecutionPolicy RemoteSigned")
+    _result = session.run_ps("Set-ExecutionPolicy RemoteSigned")
 
     logger.info(
         "Completed WorkSpaces_Automation_Windows_Windows_Updates function, returning to Step Function."
